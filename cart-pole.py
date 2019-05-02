e9a400c163cdfcb80c1719de0aec5fa5401b8cf3
Thomas Costagliola <thomas@francepronet.com>
2019-05-02 19:31:34 +0200

    --

--- cart-pole.py
+++ cart-pole.py
@@ -1,4 +1,8 @@
-import gym, numpy, sys, math, random
+import gym
+import math
+import random
+import statistics
+import tensorflow as tf
 
 env = gym.make("CartPole-v1")
 
@@ -9,8 +13,27 @@ reward_mean = []
 next_states = []
 actions = []
 
-#for epi in range(150):
-for epi in range(10):
+
+model = tf.keras.models.Sequential()
+
+model.add(tf.keras.layers.Input(shape=(4,)))
+model.add(tf.keras.layers.Dense(32, activation="relu"))
+model.add(tf.keras.layers.Dense(3, activation="linear"))
+
+model.compile(
+    loss="sparse_categorical_crossentropy",
+    optimizer="adam",
+    metrics=["accuracy"]
+)
+
+def train_model(states, actions, rewards, next_states):
+    size = len(next_states)
+
+    #tf_states = tf.tensor2d(states, shape=[states.length, 26]);
+
+
+# for epi in range(150):
+for epi in range(1):
     st = env.reset()
 
     env.render()
@@ -18,20 +41,21 @@ for epi in range(10):
     reward = 0
     step = 0
 
-    while step < 400 :
+    # while step < 400:
+    while step < 400:
 
-        #act = pickAction(st, eps)
+        # act = pickAction(st, eps)
         act = env.action_space.sample() # your agent here (this takes random actions)
 
         st2, reward, done, info = env.step(act)
 
-        if(done) :
+        if done:
             reward = int(-1)
-        else :
+        else:
             reward = int(1)
 
-        mask = [0, 0];
-        mask[act] = 1;
+        mask = [0, 0]
+        mask[act] = 1
 
         index = math.floor(random.uniform(0, 1) * len(states))
 
@@ -41,7 +65,7 @@ for epi in range(10):
         next_states.insert(index, st2)
         actions.insert(index, mask)
 
-        if (len(states) > 10000):
+        if len(states) > 10000:
             states.pop(0)
             rewards.pop(0)
             reward_mean.pop(0)
@@ -54,6 +78,13 @@ for epi in range(10):
         if done:
             break
 
-    eps = max(0.1, eps*0.99);
+    eps = max(0.1, eps*0.99)
+
+    if epi % 5 == 0:
+        print("---------------");
+        print("rewards mean", statistics.mean(reward_mean))
+        print("episode", epi);
+        train_model(states, actions, rewards, next_states)
+        #await tf.nextFrame();
 
 env.close()
